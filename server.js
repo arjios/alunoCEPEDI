12bb9ecb5d0ed7939816c08b85033b3d66cd97c3
Arjios <antonio.uab.ios.fisica@gmail.com>
2024-10-28 16:29:08 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const { insert, findAll, update, remove } = require('./repositories/alunosRepository')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    console.log('/Home')
+    res.send('Home')
+})
+
+app.get('/alunos', (req, res) => {
+    const alunos = findAll()
+    res.send(alunos)
+})
+
+app.post('/alunos', (req, res) => {
+    const { nome, email, nome_curso } = req.body
+    const aluno = insert({
+        nome,
+        email,
+        nome_curso
+    })
+    res.status(201).json(aluno)
+})
+
+app.put('/alunos/:id', (req, res) => {
+    const { id } = req.params
+    const { nome, email, nome_curso } = req.body
+    const aluno = update(id, {
+        nome,
+        email,
+        nome_curso
+    })
+    res.json(aluno)
+})
+
+app.delete('/alunos/:id', (req, res) => {
+    const { id } = req.params
+    remove(id)
+    res.status(204).send()
+})
+
+app.listen(port, () => {
+    console.log(`Server running in ${port}`)
+})
+
+
